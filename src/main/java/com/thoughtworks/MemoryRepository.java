5f03ac833f3e06bda87b4a92d99c315276e1fcc8
GraceeFlower <1491644103@qq.com>
2020-02-20 19:05:26 +0800

    [WM] feat: new the realized class MemoryRepository

---
+++ src/main/java/com/thoughtworks/MemoryRepository.java
@@ -0,0 +1,51 @@
+package com.thoughtworks;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryRepository<I> implements Repository<I> {
+
+    private Map<String, I> memoryMap = new HashMap<>();
+
+    public MemoryRepository() {
+
+    }
+
+    public MemoryRepository(Map<String, I> memoryMap) {
+        this.memoryMap = memoryMap;
+    }
+
+    public Map<String, I> getMemoryMap() {
+        return memoryMap;
+    }
+
+    public void setMemoryMap(Map<String, I> memoryMap) {
+        this.memoryMap = memoryMap;
+    }
+
+    @Override
+    public void save(String id, I entity) {
+        memoryMap.put(id, entity);
+    }
+
+    @Override
+    public I get(String id) {
+        return memoryMap.get(id);
+    }
+
+    @Override
+    public I delete(String id) {
+        return memoryMap.remove(id);
+    }
+
+    @Override
+    public void update(String id, I entity) {
+        memoryMap.replace(id, entity);
+    }
+
+    @Override
+    public Collection<I> list() {
+        return memoryMap.values();
+    }
+}
--- src/main/java/com/thoughtworks/Repository.java
+++ src/main/java/com/thoughtworks/Repository.java
@@ -1,11 +1,11 @@
 package com.thoughtworks;
 
-import java.util.List;
+import java.util.Collection;
 
 public interface Repository<I> {
     void save(String id, I entity);
-    List<I> get(String id);
-    void delete(String id);
+    I get(String id);
+    I delete(String id);
     void update(String id, I entity);
-    List<I> list();
+    Collection<I> list();
 }
